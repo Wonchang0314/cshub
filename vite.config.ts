3596af0fe1a483552e1c3e04918c04ab2f5c9b07
Wonchang0314 <andrew0314@naver.com>
2025-01-11 21:11:33 +0900

    FIX: 외부 모듈 설정 제거

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,4 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  build: {
-    rollupOptions: {
-      external: ["framer-motion", "styled-components"],
-    },
-  },
-  define: {
-    "global.React": "React",
-    "global.ReactDOM": "ReactDOM",
-    "global.FramerMotion": "window.FramerMotion",
-    "global.styled": "window.styled",
-  },
 });
